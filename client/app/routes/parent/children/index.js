dbbb2a59dfd12d32f935183169c7d576543f7962
jame <jame.mackson@gmail.com>
2015-04-17 16:14:39 -0500

    added example of creating child within existing route.

--- client/app/routes/parent/children/index.js
+++ client/app/routes/parent/children/index.js
@@ -9,5 +9,43 @@ export default Ember.Route.extend({
     var parent = this.parent;
     var children = this.store.find('child', { parent: parent.id });
     return children;
+  },
+  newChildIsValid: function() {
+    //overly simplified validation mechanism.
+    return ($('#new-client-name').val() != '' && $('#new-client-age').val() != '');
+  },
+  actions: {
+    createChild: function() {
+      //check if form data is valid
+      if (this.newChildIsValid()) {
+        var self = this;
+        //create the child record itself
+        var child = this.store.createRecord('child', {
+          name : $('#new-client-name').val(),
+          age : $('#new-client-age').val()
+        });
+        //set the parent so it is associated to the current parent
+        child.set('parent', this.get('parent'));
+        //save the child record to the backend
+        return child.save().then(
+          function(child) {           // success callback
+            //clear the input fields
+            $('#new-client-name').val('');
+            $('#new-client-age').val('');
+            //refresh the current route so the new child record is displayed
+            self.refresh();
+          },
+          function(reason) {          // failure callback
+            console.log('error creating child: ' + reason);
+            alert('error creating child: ' + reason);
+            //refresh the current route, just in case... :)
+            self.refresh();
+          }
+        );
+      } else {
+        //alert the user that they need to fill in the form.
+        alert('please enter the child\'s name and age');
+      }
+    }
   }
 });
