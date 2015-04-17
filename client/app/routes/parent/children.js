eb21ccfce94ea482c75282b457156ebc46c3b6cf
jame <jame.mackson@gmail.com>
2015-04-17 09:27:44 -0500

    moved model hook from children to children.index

--- client/app/routes/parent/children.js
+++ client/app/routes/parent/children.js
@@ -1,13 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  parent: null,
-  beforeModel: function() {
-    this.set('parent', this.modelFor('parent'));
-  },
-  model: function(params) {
-    var parent = this.parent;
-    var children = this.store.find('child', { parent: parent.id });
-    return children;
-  }
+
 });
--- client/app/routes/parent/children/index.js
+++ client/app/routes/parent/children/index.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  parent: null,
+  beforeModel: function() {
+    this.set('parent', this.modelFor('parent'));
+  },
+  model: function(params) {
+    var parent = this.parent;
+    var children = this.store.find('child', { parent: parent.id });
+    return children;
+  }
 });
